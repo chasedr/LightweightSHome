26d1b93357eff090be372c1d94ff834af9e3ec89
jarvis <gcy031295@163.com>
2017-07-26 20:01:08 +0800

    [add][jarvis] 1、添加风扇和灯光的控制脚本，只实现了基本的参数检查功能和返回操作码功能

---
+++ Embedded PT/setfanopt.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+import sys
+
+argvlen = len(sys.argv)
+if argvlen-1 != 4:
+	print "less params"
+	sys.exit("ERR")
+if sys.argv[1] == "-address":
+	addr = sys.argv[2]
+else:
+	print "first option error."
+	sys.exit("ERR")
+
+if sys.argv[3] == "-speed":
+	speed = sys.argv[4]
+else:
+	print "second option error"
+	sys.exit("ERR")
+
+sys.exit("OK")
+
+
