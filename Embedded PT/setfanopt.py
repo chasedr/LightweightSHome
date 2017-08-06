ba5cfdf1190b082db8fb8da55b70abed7f41ea00
jarvis <gcy031295@163.com>
2017-08-06 20:31:45 +0800

    [modify][jarvis] 1、修改两个控制脚本，使得可以控制连接在服务器上的远程设备 2、修改温度光照度服务器，解决无法写入数据库的BUG [new][jarvis] 1、新增控制服务器，使得能够控制网络上所有在线的执行设备

--- Embedded PT/setfanopt.py
+++ Embedded PT/setfanopt.py
@@ -2,11 +2,17 @@
 #-*- coding:utf-8 -*-
 
 import sys
+import socket
+import os
 
 argvlen = len(sys.argv)
+addr = ""
+speed = ""
+
 if argvlen-1 != 4:
 	print "less params"
 	sys.exit("ERR")
+
 if sys.argv[1] == "-address":
 	addr = sys.argv[2]
 else:
@@ -18,6 +24,11 @@ if sys.argv[3] == "-speed":
 else:
 	print "second option error"
 	sys.exit("ERR")
+
+client = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
+client.connect("/tmp/temp.sock")
+client.send("address and speed :"+addr+","+speed)
+client.close()
 
 sys.exit("OK")
 
